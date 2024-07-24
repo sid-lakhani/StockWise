51cc0a2f475b9720b2dbd875401e55e3aa80938f
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-07-24 23:49:30 +0530

    feat: Bento Grid

--- src/components/dashboard/Bento.tsx
+++ src/components/dashboard/Bento.tsx
@@ -5,29 +5,38 @@ import { DollarSign, FileCheck2, Newspaper, TrendingUp } from "lucide-react";
 const features = [
   {
     Icon: TrendingUp,
-    name: "Buy Stocks",
-    description: "Easily purchase stocks and track your investments.",
-    href: "/dashboard/stocks",
+    name: "Market Overview",
+    description: "Real-time data on the latest stock market trends.",
+    href: "",
     cta: "Learn more",
-    className: "col-span-3 lg:col-span-1",
+    className: "row-span-2 col-span-3 lg:col-span-2",
     background: "",
   },
   {
     Icon: FileCheck2,
     name: "My Portfolio",
     description: "Monitor and manage your stock portfolio efficiently.",
-    href: "/dashboard/portfolio",
+    href: "/dashboard/stocks",
     cta: "Learn more",
-    className: "col-span-3 lg:col-span-2",
+    className: "row-span-1 col-span-3 lg:col-span-1",
     background: "",
   },
   {
     Icon: Newspaper,
     name: "News",
-    description: "Stay updated with the latest market news.",
+    description: "Easily purchase stocks and track your investments.",
     href: "/dashboard/news",
     cta: "Learn more",
-    className: "col-span-3 lg:col-span-2",
+    className: "row-span-2 col-span-3 lg:col-span-1",
+    background: "",
+  },
+  {
+    Icon: Newspaper,
+    name: "News",
+    description: "Stay updated with the latest market news.Easily purchase stocks and track your investments.",
+    href: "/dashboard/news",
+    cta: "Learn more",
+    className: "col-span-3 lg:col-span-1",
     background: "",
   },
   {
@@ -43,7 +52,7 @@ const features = [
 
 export async function Bento() {
   return (
-    <BentoGrid className="lg:grid-rows-3 pt-32 h-[100dvh] grid w-2/3">
+    <BentoGrid className="lg:grid-rows-3 grid-cols-3 pt-28 h-[95dvh] grid w-2/3">
       {features.map((feature) => (
         <BentoCard key={feature.name} {...feature} />
       ))}
