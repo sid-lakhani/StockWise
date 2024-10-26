a62d639d1aedd567ae96d06dfe931ecb2544b1a5
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-10-26 12:45:05 +0530

    fix: result test

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <h1>Stock Symbol Search and Data</h1>
 
       <SymbolSearch onSelectSymbol={setSelectedSymbol} />
-
+      <div className='h-[2px] my-8 bg-black dark:bg-white'></div>
       {selectedSymbol ? (
         <StockData symbol={selectedSymbol} />
       ) : (
