28f1629b94a00a42b1faaf099338a3d6547708c5
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-10-28 15:20:15 +0530

    fix:

--- src/components/dashboard/Bento.tsx
+++ src/components/dashboard/Bento.tsx
@@ -3,7 +3,7 @@ import { bentoelements } from "@/constants/bentoelements";
 
 export default function Bento() {
   return (
-    <BentoGrid className="md:grid-rows-3 md:grid-cols-3 pt-16 md:pt-28 h-[95dvh] grid md:w-2/3 mx-6 md:mx-0">
+    <BentoGrid className="md:grid-rows-3 md:grid-cols-3 pt-28 h-[95dvh] grid md:w-2/3 mx-6 md:mx-0">
       {bentoelements.map((feature) => (
         <BentoCard key={feature.name} {...feature} />
       ))}
