0ac9322d1e2511143e96426ffea7a687477bac70
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-06-03 20:06:05 +0530

    removed unnecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { ClerkProvider, SignedOut } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
